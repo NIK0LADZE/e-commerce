686025579b1c5c6b1f99ffd471a9a11f522c132c
NIK0LADZE <g.nikoladze13@gmail.com>
2021-12-31 22:58:35 +0400

    Header position was set to fixed

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -7,11 +7,11 @@ import classes from "./Header.module.css";
 class Header extends React.Component {
   render() {
     return (
-      <div className={classes.row}>
+      <header className={classes.row}>
         <Navigation categories={this.props.categories} activeCategory={this.props.activeCategory} />
         <img src={Logo} className={classes.logo} alt="logo" />
         <Actions />
-      </div>
+      </header>
     );
   }
 }
