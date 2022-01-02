973fdeb38f49322d35a782e0fe23b9573f503954
NIK0LADZE <g.nikoladze13@gmail.com>
2022-01-02 07:23:54 +0400

    CurrencyProvider was applied to ProductItem component

--- src/Components/Products/ProductItem/ProductItem.js
+++ src/Components/Products/ProductItem/ProductItem.js
@@ -1,12 +1,20 @@
 import React from "react";
+import CurrencyContext from "../../../store/CurrencyContext";
 import cartIcon from "../../../assets/cart-white.svg";
 import classes from "./ProductItem.module.css";
+import ErrorIcon from "../../UI/ErrorIcon/ErrorIcon";
 
 class ProductItem extends React.Component {
+  static contextType = CurrencyContext;
+
   render() {
     let product = this.props.product;
     return (
-      <li className={`${classes.productCard} ${product.inStock ? classes.inStock : ""}`}>
+      <li
+        className={`${classes.productCard} ${
+          product.inStock && !this.context.error ? classes.available : ""
+        }`}
+      >
         <div className={classes.photoContainer}>
           <img className={classes.productImage} src={product.gallery[0]} alt={product.name} />
           <div className={classes.cartButton}>
@@ -20,15 +28,15 @@ class ProductItem extends React.Component {
         </div>
         <p className={classes.productName}>{product.name}</p>
         <p className={classes.price}>
-          {
-            // Finds selected currency symbol
-            product.prices.find((currencyObj) => currencyObj.currency.label === "USD").currency
-              .symbol
-          }
-          {
-            // Finds selected currency amount
-            product.prices.find((currencyObj) => currencyObj.currency.label === "USD").amount
-          }
+          {/* Error message */}
+          {this.context.error && <ErrorIcon />}
+          {this.context.error && <span className={classes.errorMessage}>{this.context.error}</span>}
+          {!this.context.error && this.context.selectedCurrencySymbol}
+          {/* Finds selected currency amount */}
+          {!this.context.error &&
+            product.prices.find(
+              (currencyObj) => currencyObj.currency.label === this.context.selectedCurrency
+            ).amount}
         </p>
       </li>
     );
