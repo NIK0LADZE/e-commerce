08257569eac76fdf98f81adaae24dd84695f9ecc
NIK0LADZE <g.nikoladze13@gmail.com>
2022-01-03 19:01:27 +0400

    Customer has to select attributes before adding product to cart

--- src/Components/ProductPage/ProductInfo/ProductInfo.js
+++ src/Components/ProductPage/ProductInfo/ProductInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CurrencyContext from "../../../store/CurrencyContext";
+import AttributeSelector from "../../UI/AttributeSelector/AttributeSelector";
 import Button from "../../UI/Button/Button";
 import ErrorIcon from "../../UI/ErrorIcon/ErrorIcon";
 import classes from "./ProductInfo.module.css";
@@ -7,8 +8,23 @@ import classes from "./ProductInfo.module.css";
 class ProductInfo extends React.Component {
   static contextType = CurrencyContext;
 
+  state = { selectedAttributes: {} };
+
+  onSelectAttribute = (attributeName, attributeValue) => {
+    const selectedAttributeObj = { [attributeName]: attributeValue };
+    this.setState({
+      selectedAttributes: { ...this.state.selectedAttributes, ...selectedAttributeObj },
+    });
+  };
+
   render() {
     const currentCurrency = this.context.selectedCurrency;
+    const objectKeys = Object.keys(this.state.selectedAttributes);
+    // Finds selected currency amount
+    const price = this.props.product.prices.find(
+      (priceObj) => priceObj.currency.label === currentCurrency
+    ).amount;
+    const canAddToCart = this.props.product.attributes.length === objectKeys.length;
 
     return (
       <div className={classes.info}>
@@ -23,41 +39,35 @@ class ProductInfo extends React.Component {
               <ul className={classes.attributeList}>
                 {attribute.items.map((item) => {
                   return (
-                    <li
-                      key={item.displayValue}
-                      className={classes.attributeItem}
-                      style={{
-                        backgroundColor: `${attribute.type === "swatch" ? item.value : ""}`,
-                      }}
-                    >
-                      {attribute.type !== "swatch" && (
-                        <p className={classes.itemValue}>{item.value}</p>
-                      )}
-                    </li>
+                    <AttributeSelector
+                      key={item.id}
+                      selectAttribute={this.onSelectAttribute}
+                      item={item}
+                      attributeName={attribute.name}
+                      attributeType={attribute.type}
+                      state={this.state.selectedAttributes}
+                    />
                   );
                 })}
               </ul>
             </div>
           );
         })}
+
         <div>
           <p className={classes.attributeName}>price:</p>
           <p className={classes.price}>
-            {currentCurrency &&
-              this.props.product.prices.find(
-                (priceObj) => priceObj.currency.label === currentCurrency
-              ).currency.symbol}
-            {currentCurrency &&
-              this.props.product.prices.find(
-                (priceObj) => priceObj.currency.label === currentCurrency
-              ).amount}
+            {currentCurrency && this.context.selectedCurrencySymbol}
+            {currentCurrency && price}
             {!currentCurrency && <ErrorIcon />}
             <span className={classes.errorMessage}>{!currentCurrency && this.context.error}</span>
           </p>
         </div>
+
         <div className={classes.buttonContainer}>
-          <Button disabled={!currentCurrency}>add to cart</Button>
+          <Button disabled={!currentCurrency || !canAddToCart}>add to cart</Button>
         </div>
+        {/* Product Description */}
         <div
           className={classes.description}
           dangerouslySetInnerHTML={{ __html: this.props.product.description }}
---
+++ src/Components/UI/AttributeSelector/AttributeSelector.js
@@ -0,0 +1,43 @@
+import React from "react";
+import checkIcon from "../../../assets/checkIcon.svg";
+import classes from "./AttributeSelector.module.css";
+
+class AttributeSelector extends React.Component {
+  render() {
+    const item = this.props.item;
+    const attributeName = this.props.attributeName;
+    const attributeType = this.props.attributeType;
+    const swatchBg = attributeType === "swatch" ? item.value : "";
+    const attributeSelected = this.props.state[attributeName] === item.displayValue;
+
+    return (
+      <li
+        key={item.id}
+        className={classes.attributeItem}
+        style={{
+          backgroundColor: swatchBg,
+        }}
+        onClick={() => this.props.selectAttribute(attributeName, item.displayValue)}
+      >
+        {attributeType === "swatch" && (
+          <div
+            className={`${classes.swatchAttribute} ${
+              attributeSelected ? classes.swatchSelected : ""
+            }`}
+          >
+            {attributeSelected && (
+              <img className={classes.checkIcon} src={checkIcon} alt="Check icon" />
+            )}
+          </div>
+        )}
+        {attributeType !== "swatch" && (
+          <p className={`${classes.itemValue} ${attributeSelected ? classes.selected : ""}`}>
+            {item.value}
+          </p>
+        )}
+      </li>
+    );
+  }
+}
+
+export default AttributeSelector;
